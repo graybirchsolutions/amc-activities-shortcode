0443eb96b04abe2ddd35f6dd73e70d0c41a55d12
Marty Jensen <marty@graybirch.solutions>
2023-05-02 11:48:06 -0400

    Issue #16: Correct image display problem.

--- assets/js/amcActivitiesRenderEvents.js
+++ assets/js/amcActivitiesRenderEvents.js
@@ -260,8 +260,9 @@ const renderEvent = function (activity) {
   details.className = 'amc-event-details'
 
   const img = document.createElement('img')
-  if (!activity.trip_images.length === 0) {
+  if (activity.trip_images.length > 0) {
     img.src = `https:${activity.trip_images[0]}`
+    img.maxwidth = '200'
   } else {
     // Fetch a random seeded image from our own assets
     img.src = `${AMC_ACTIVITIES_ASSETDIR_URL}/img/AMC_Logo_${Math.floor(Math.random() * 10) + 1}.svg`
